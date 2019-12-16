147c9a782cb6a93ed28654c715c93acd33f94fae
ChrisEng0212 <cjx02121981@gmail.com>
2019-12-16 23:10:16 +0800

    app_config

--- app.py
+++ app.py
@@ -3,11 +3,23 @@ from flask_sqlalchemy import SQLAlchemy  #needed for app initialization (see bel
 from flask_bcrypt import Bcrypt  #needed for password storage
 from flask_login import LoginManager, current_user #needed for login
 import os
-
+try:
+    from config import BaseConfig
+    DEBUG = BaseConfig.DEBUG
+    SECRET_KEY = BaseConfig.SECRET_KEY
+    SQLALCHEMY_DATABASE_URI = BaseConfig.SQLALCHEMY_DATABASE_URI    
+except:
+    DEBUG = os.environ['DEBUG']
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+   
 
 
 app = Flask(__name__)
-app.config.from_object('config.BaseConfig')
+app.config['DEBUG'] = DEBUG
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI    
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt()
 login = LoginManager(app)
